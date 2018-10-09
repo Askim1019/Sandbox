4644ffa1e8555fa681a97a912448e774525b6799
Alex Kim <akim@futurefinance.com>
2018-10-09 13:49:52 -0500

    Add some more tests, create RPN class

--- rpn_calculator/spec/rpn_calculator_spec.rb
+++ rpn_calculator/spec/rpn_calculator_spec.rb
@@ -1,11 +1,34 @@
 require 'rspec'
 require 'pry'
 
+class RPNCalculator
+  def self.process(expression)
+  end
+end
+
 RSpec.describe 'RPN Calculator' do
-  it 'adds two numbers together' do
+  it 'adds two numbers ' do
     result = RPNCalculator.process("(1 2 +)")
 
     expect(result).to eq(3)
+  end
+
+  xit 'subtracts two numbers' do
+    result = RPNCalculator.process("(6 2 -)")
+
+    expect(result).to eq(4)
+  end
+
+  xit 'multiplies two numbers' do
+    result = RPNCalculator.process("(3 6 *)")
+
+    expect(result).to eq(18)
+  end
+
+  xit 'divides two numbers' do
+    result = RPNCalculator.process("(14 7 /")
+
+    expect (result).to eq(2)
   end
 
   xit 'operates with multiple operand' do
