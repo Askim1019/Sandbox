dcd409fe281df65c04746016ab4c6f53233b4f7b
Alex Kim <akim@futurefinance.com>
2018-10-09 13:28:03 -0500

    Write test 2

--- rpn_calculator/spec/rpn_calculator_spec.rb
+++ rpn_calculator/spec/rpn_calculator_spec.rb
@@ -2,6 +2,15 @@ require 'rspec'
 require 'pry'
 
 RSpec.describe 'RPN Calculator' do
-  xit 'adds two numbers together' do
+  it 'adds two numbers together' do
+    result = RPNCalculator.process("(1 2 +)")
+
+    expect(result).to eq(3)
+  end
+
+  xit 'operates with multiple operand' do
+    result = RPNCalculator.process("(1 2 + 4 +)")
+
+    expect(result).to eq(7)
   end
 end
